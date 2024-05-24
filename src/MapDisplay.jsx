2bdb160cb143edcf02624bd1c855aaaff204d0a1
TTTTTaiki <itoutaiki418@gmail.com>
2024-05-24 22:16:25 +0900

    volcanoデータのimport

--- src/MapDisplay.jsx
+++ src/MapDisplay.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import * as d3 from "d3";
 import geoJson from "./assets/Japan.json";
+import volcano from "./assets/volcano.json";
+
 
 export const MapDisplay = () => {
-  console.log();
+  console.log(volcano);
   return (
     <div id="map-container" style={{ width: "100%", height: "400px" }}></div>
   );
