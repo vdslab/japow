e305dc814fc188d0daf5bc276bcacf38bed9edde
S-yshzk <chsu21046@g.nihon-u.ac.jp>
2024-05-28 01:26:40 +0900

    地図にプロット

--- src/MapDisplay.jsx
+++ src/MapDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import * as d3 from "d3";
 import JapanData from "./assets/Japan.json";
+import zahyou from "./assets/snowlev-2023020212.json";
 
 const ZoomableSVG = (props) => {
   const { children } = props;
@@ -9,10 +10,12 @@ const ZoomableSVG = (props) => {
   const [k, setK] = useState(1);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-
+  const minZoom = 0.7;
+  const maxZoom = 2;
   useEffect(() => {
-    const zoom = d3.zoom().on("zoom", (event) => {
+    const zoom = d3.zoom().scaleExtent([minZoom, maxZoom]).on("zoom", (event) => {
       const { x, y, k } = event.transform;
+      console.log(x, y, k);
       setX(x);
       setY(y);
       setK(k);
@@ -21,17 +24,21 @@ const ZoomableSVG = (props) => {
   }, []);
   return (
     <svg ref={svgRef} width={window.innerWidth} height={window.innerHeight}>
-      <g transform={`translate(${x + 300},${y + 200})scale(${k - 0.5})`}>
+      <g transform={`translate(${x + 150},${y})scale(${k - 0.5})`}>
         {children}
       </g>
     </svg>
   );
 };
 
+const plot = () => {
+
+}
+
 const MapDisplay = () => {
   const svgRef = useRef(null);
   const width = 800;
-  const height = 600;
+  const height = 800;
   // const [tooltipContent, setTooltipContent] = useState("");
   var svg;
 
@@ -39,12 +46,13 @@ const MapDisplay = () => {
     const projection = d3
       .geoMercator()
       .center([137, 38]) // 日本の中心座標
-      .scale(1000) // スケール調整
+      .scale(2000) // スケール調整
       .translate([width / 2, height / 2]);
 
     const path = d3.geoPath().projection(projection);
 
     svg = d3.select(svgRef.current).attr("width", width).attr("height", height);
+
 
     svg
       .selectAll("path")
@@ -54,6 +62,30 @@ const MapDisplay = () => {
       .attr("d", path)
       .attr("fill", "#ccc")
       .attr("stroke", "#000");
+
+    // 点を描画
+    svg.append('g')
+      .selectAll('circle')
+      .data(zahyou)
+      .enter()
+      .append('circle')
+      .attr('cx', d => {
+        const coords = projection([d.longitude, d.latitude]);
+        return coords ? coords[0] : null;
+      })
+      .attr('cy', d => {
+        const coords = projection([d.longitude, d.latitude]);
+        return coords ? coords[1] : null;
+      })
+      .attr('r', 0.5)
+      .attr('fill', 'red')
+      .on("click", (event, d) => {
+        // クリックイベントのハンドラ
+        console.log("Clicked data:", d);
+        // d3.select(event.currentTarget).attr("fill", "blue");
+      });
+
+      
   }, []);
 
   return (
