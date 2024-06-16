cc71329a722c482823c1d055b63ed935903077e0
S-yshzk <chsu21046@g.nihon-u.ac.jp>
2024-06-17 01:26:54 +0900

    不要なプロパティ削除

--- docker-python/src/snow.py
+++ docker-python/src/snow.py
@@ -1,55 +1,55 @@
 import pygrib
+import pandas as pd
 import json
-import numpy as np
 
-# GRIBファイルを開く（編集したいファイル名)
-filename = '/grib2/anl_snwlev.2023020812'
+# GRIBファイルを開く
+filename = '/grib2/anl_snwlev.2023020212'
 grbs = pygrib.open(filename)
 
+# 本州、四国、九州などの主要な部分
+MAINLAND_LAT_MIN = 30  # 北緯30度から
+MAINLAND_LAT_MAX = 41  # 北緯41度まで
+MAINLAND_LON_MIN = 128  # 東経128度から
+MAINLAND_LON_MAX = 142  # 東経142度まで
 
-# 日本の緯度経度範囲（北緯20度から45度、東経122度から153度）
-japan_lat_min = 20
-japan_lat_max = 46
-japan_lon_min = 120
-japan_lon_max = 150
+# 北海道
+HOKKAIDO_LAT_MIN = 41  # 北緯41度から
+HOKKAIDO_LAT_MAX = 46  # 北緯46度まで
+HOKKAIDO_LON_MIN = 139  # 東経139度から
+HOKKAIDO_LON_MAX = 145  # 東経145度まで
 
-# JSONファイルを開く (データを入れるファイル名)
-output_filename = '/data/snowlev-2023020812.json'
+parameterName = ["snowag", "snowlayers", "snowAlbedoAgainstVisible", "snowAlbedoForNear-infrared", "t1", "t2", "t3", "t4", "snowMassDensity1", "snowMassDensity2", "snowMassDensity3", "snowMassDensity4", "WaterContent1", "WaterContent2", "WaterContent3", "WaterContent4", "iceContent1", "iceContent2", "iceContent3", "iceContent4"]
+
+
+# JSONファイルを開く
+output_filename = '/src/index.json'
 with open(output_filename, 'w') as json_file:
     # JSON配列の開始
     json_file.write('[\n')
 
     # 座標ごとのデータを収集するための辞書
     data_dict = {}
-    
-    
 
-    # ファイル内の全てのメッセージをイテレートして詳細情報を取得
+    # 最初のメッセージから緯度経度を取得
+    lat, lon = grbs.message(1).latlons()
+    latLength = lat.shape[0]
+    lonLength = lon.shape[1]
     for grb in grbs:
-        lat, lon = grb.latlons()
         values = grb.values
-
-
-        # 日本の範囲内のデータのみ処理
-        for i in range(len(lat)):
-            for j in range(len(lat[i])):
-                if japan_lat_min <= lat[i, j] <= japan_lat_max and japan_lon_min <= lon[i, j] <= japan_lon_max:
-                    data_value = values[i, j]
-                    # 欠損値（--）の処理
-                    if data_value != '--':
-                        coord_key = (lat[i, j], lon[i, j])
+        for i in range(latLength):
+            for j in range(lonLength):
+                if MAINLAND_LAT_MIN <= lat[i][j] <= MAINLAND_LAT_MAX and MAINLAND_LON_MIN <= lon[i][j] <= MAINLAND_LON_MAX or HOKKAIDO_LAT_MIN <= lat[i][j] <= HOKKAIDO_LAT_MAX and HOKKAIDO_LON_MIN <= lon[i][j] <= HOKKAIDO_LON_MAX:
+                    value = values[i][j]
+                    if value != '__':
+                        coord_key = (lat[i][j], lon[i][j])
                         if coord_key not in data_dict:
                             data_dict[coord_key] = {
-                                "latitude": lat[i, j],
-                                "longitude": lon[i, j],
-                                "values": []
+                                "latitude": lat[i][j],
+                                "longitude": lon[i][j],
+                                "values": {}
                             }
-                        data_dict[coord_key]["values"].append({
-                            "message_number": grb.messagenumber,
-                            "short_name": grb.shortName,
-                            "data_value": data_value
-                        })
-
+                        data_dict[coord_key]["values"][parameterName[grb.messagenumber - 1]] = value
+                
     # 辞書からJSONファイルに書き込み
     first_entry = True
     for coord, data in data_dict.items():
@@ -62,7 +62,5 @@ with open(output_filename, 'w') as json_file:
     # JSON配列の終了
     json_file.write('\n]')
 
-# ファイルを閉じる
-grbs.close()
 
 print("Data has been stored in", output_filename)
