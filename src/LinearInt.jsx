b4bdb350de121217d64fd3a5cfa4f989035036c6
Kawabe Kaito <chka21055@g.nihon-u.ac.jp>
2024-06-18 07:32:08 +0900

    周りに四点なかった場合の処理を追加

--- src/LinearInt.jsx
+++ src/LinearInt.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import skiPoint from "./assets/sukijou-zahyou.json";
-import gridzahyou from "../docker-python/data/snowlev-2023020812.json";
+import gridzahyou from "../docker-python/data/snowlev-2023020212.json";
 
 // "region": "山形",
 // "name": "蔵王温泉スキー場",
@@ -26,7 +26,7 @@ const LinearInt = () => {
   const findPoints = (skiPoint, grid) => {
     const xSorted = grid.slice().sort((a, b) => a.latitude - b.latitude);
     const ySorted = grid.slice().sort((a, b) => a.longitude - b.longitude);
-
+    // console.log(ySorted);
     let x0, x1, y0, y1;
 
     for (let i = 0; i < xSorted.length - 1; i++) {
@@ -55,15 +55,11 @@ const LinearInt = () => {
       return [];
     }
 
-    const skiInfo = {
+    const points = {
       region: skiPoint.region,
       name: skiPoint.name,
       latitude: skiPoint.latitude,
       longitude: skiPoint.longitude,
-    };
-
-    const points = {
-      ...skiInfo,
       surroundingPoints: [
         grid.find(
           (p) => p.latitude === x0.latitude && p.longitude === y0.longitude
@@ -81,27 +77,72 @@ const LinearInt = () => {
     };
 
     if (points.surroundingPoints.includes(undefined)) {
-      return {
-        ...skiInfo,
-        surroundingPoints: null,
-      };
-    } else {
-      return points;
+      const R = Math.PI / 180;
+      function distance(lat1, lng1, lat2, lng2) {
+        lat1 *= R;
+        lng1 *= R;
+        lat2 *= R;
+        lng2 *= R;
+        return (
+          6371 *
+          Math.acos(
+            Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1) +
+              Math.sin(lat1) * Math.sin(lat2)
+          )
+        );
+      }
+
+      var firstNear, firstIndex;
+      var secondNear, secondIndex;
+
+      var firstResult;
+      for (let i = 0; i < xSorted.length - 1; i++) {
+        var result = distance(
+          xSorted[i].latitude,
+          xSorted[i].longitude,
+          skiPoint.latitude,
+          skiPoint.longitude
+        );
+        if (i === 0) {
+          firstResult = result;
+          firstIndex = i;
+          firstNear = [
+            xSorted[firstIndex].latitude,
+            xSorted[firstIndex].longitude,
+          ];
+        }
+        if (result < firstResult) {
+          firstResult = result;
+          secondIndex = firstIndex;
+          firstIndex = i;
+          secondNear = firstNear;
+          firstNear = [
+            xSorted[firstIndex].latitude,
+            xSorted[firstIndex].longitude,
+          ];
+        }
+      }
+
+      points.surroundingPoints = [xSorted[firstIndex], xSorted[secondIndex]];
+      // console.log(points);
     }
+    return points;
   };
 
   //左下p00,右下p10,左上p01,右上p11
   const bilinearInt = (skiPoint) => {
-    if (skiPoint.surroundingPoints !== null) {
-      const x = skiPoint.latitude;
-      const y = skiPoint.longitude;
+    const x = skiPoint.latitude;
+    const y = skiPoint.longitude;
+    var dataResult = [];
+    if (skiPoint.surroundingPoints.length === 4) {
+      //周りの四点で線形補間
       const [p00, p10, p01, p11] = skiPoint.surroundingPoints;
 
       const x0 = p00.latitude,
         y0 = p00.longitude;
       const x1 = p10.latitude,
         y1 = p11.longitude;
-      var dataResult = [];
+
       for (let i = 0; i < p00.values.length; i++) {
         const f00 = p00.values[i].data_value;
         const f10 = p10.values[i].data_value;
@@ -119,6 +160,25 @@ const LinearInt = () => {
             ((x1 - x0) * (y1 - y0)),
         };
 
+        dataResult.push(result);
+      }
+      return {
+        ...skiPoint,
+        values: dataResult,
+      };
+    } else if (skiPoint.surroundingPoints.length === 2) {
+      //周りに四点なかった場合。二点で線形補間
+      const [p0, p1] = skiPoint.surroundingPoints;
+      for (let i = 0; i < p0.values.length; i++) {
+        const f0 = p0.values[i].data_value;
+        const f1 = p1.values[i].data_value;
+        const result = {
+          message_number: p1.values[i].message_number,
+          short_name: p1.values[i].short_name,
+          data_value:
+            (f0 * (p1.latitude - x) + f1 * (x - p0.latitude)) /
+            (p1.latitude - p0.latitude),
+        };
         dataResult.push(result);
       }
 
