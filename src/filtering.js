bdf0acf26ca47766d746c74bd1c9b0b65e45ca68
S-yshzk <chsu21046@g.nihon-u.ac.jp>
2024-07-09 09:50:11 +0900

    都道府県フィルタリング実装

--- src/filtering.js
+++ src/filtering.js
@@ -1,10 +1,13 @@
 export const snowFilterBypref = (data, slectedPref) => {
-    debugger;
+    // debugger;
     return (
         data.map((month) => {
-            return (
-                month.values.filter(({ region }) => region === slectedPref)
-            );
+            month.values = month.values.filter(({ region }) => region === slectedPref);
+            month.weeks = month.weeks.map((week) => {
+                week.values = week.values.filter(({ region }) => region === slectedPref);
+                return week;
+            })
+            return month;
         })
     )
 }
