4fa3d412a9fd03cc39fbff4c6bbe415caa1c5629
Kawabe Kaito <chka21055@g.nihon-u.ac.jp>
2024-07-22 22:13:10 +0900

    風のデータも追加

--- src/functions/LinearInt.js
+++ src/functions/LinearInt.js
@@ -206,7 +206,7 @@ const FindNearPoint = (skiPoint, nearest, xSorted) => {
 
 const makeJsonFile = (data, outputFileName) => {
   const jsonData = data;
-  const outputFolderPath = "./data";
+  const outputFolderPath = "../dataWithFourPoints";
   const fileNameWithJson = `${outputFileName}.json`;
   const outputPath = path.join(outputFolderPath, fileNameWithJson);
   const blobData = JSON.stringify(jsonData, null, 2);
