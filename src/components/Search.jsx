2a335be6b975eb743bacd3bfaf68997c4f42f5ef
Kawabe Kaito <chka21055@g.nihon-u.ac.jp>
2024-12-19 00:07:30 +0900

    ぬ

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,12 +7,70 @@ import options from "../assets/ski_resorts_japan.json";
 import { InputAdornment } from "@mui/material";
 import Box from "@mui/material/Box";
 
+const prefectureOrder = [
+  "北海道",
+  "青森県",
+  "岩手県",
+  "宮城県",
+  "秋田県",
+  "山形県",
+  "福島県",
+  "茨城県",
+  "栃木県",
+  "群馬県",
+  "埼玉県",
+  "千葉県",
+  "東京都",
+  "神奈川県",
+  "新潟県",
+  "富山県",
+  "石川県",
+  "福井県",
+  "山梨県",
+  "長野県",
+  "岐阜県",
+  "静岡県",
+  "愛知県",
+  "三重県",
+  "滋賀県",
+  "京都府",
+  "大阪府",
+  "兵庫県",
+  "奈良県",
+  "和歌山県",
+  "鳥取県",
+  "島根県",
+  "岡山県",
+  "広島県",
+  "山口県",
+  "徳島県",
+  "香川県",
+  "愛媛県",
+  "高知県",
+  "福岡県",
+  "佐賀県",
+  "長崎県",
+  "熊本県",
+  "大分県",
+  "宮崎県",
+  "鹿児島県",
+  "沖縄県",
+];
+
 const Search = ({ skiTargetID, setSkiTargetID }) => {
-  const [filteredOptions, setFilteredOptions] = useState(options);
+  const [filteredOptions, setFilteredOptions] = useState([]);
   const [searchInput, setSearchInput] = useState([]);
   const [selectedOptions, setSelectedOptions] = useState([]);
 
   useEffect(() => {
+    // 都道府県順でソート
+    const sortedOptions = [...options].sort((a, b) => {
+      const regionA = prefectureOrder.indexOf(a.region);
+      const regionB = prefectureOrder.indexOf(b.region);
+      return regionA - regionB;
+    });
+    setFilteredOptions(sortedOptions);
+
     const selected = skiTargetID
       ? options.filter((option) => skiTargetID.includes(option.skiID))
       : [];
@@ -43,6 +101,16 @@ const Search = ({ skiTargetID, setSkiTargetID }) => {
         getOptionLabel={(option) => (option && option.name) || ""}
         value={selectedOptions}
         onChange={handleOptionChange}
+        renderOption={(props, option) => (
+          <li {...props}>
+            <Box>
+              <div>{option.name}</div>
+              <div style={{ fontSize: "0.8rem", color: "gray" }}>
+                {option.region}
+              </div>
+            </Box>
+          </li>
+        )}
         renderInput={(params) => (
           <TextField
             {...params}
@@ -50,14 +118,7 @@ const Search = ({ skiTargetID, setSkiTargetID }) => {
             label="Search Ski Resorts"
             InputProps={{
               ...params.InputProps,
-              endAdornment: (
-                <>
-                  {params.InputProps.endAdornment}
-                  <InputAdornment position="end">
-                    <IconButton onClick={handleSearchClick}></IconButton>
-                  </InputAdornment>
-                </>
-              ),
+              endAdornment: <>{params.InputProps.endAdornment}</>,
               disableUnderline: true,
             }}
             sx={{
